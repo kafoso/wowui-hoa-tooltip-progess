d3f9b5785f59e207a2680724afa637b41b29fe14
kafoso <soefritz@gmail.com>
2018-09-01 20:33:02 +0200

    Initial commit

---
+++ Interface/AddOns/HeartOfAzerothTooltip/main.lua
@@ -0,0 +1,33 @@
+local INVENTORY_SLOT_ID_NECK = 2
+local HEART_OF_AZEROTH_ITEM_ID = 158075
+
+function HeartOfAzerothTooltip_NumberFormat(number, decimalCount)
+	local multiplier = 10^(decimalCount or 0)
+	return math.floor(number * multiplier + 0.5) / multiplier
+end
+
+function HeartOfAzerothTooltip_GetAzeriteProgressString()
+	local azeriteItemLocation = C_AzeriteItem.FindActiveAzeriteItem()
+	local str = ""
+	if (C_AzeriteItem.HasActiveAzeriteItem() and azeriteItemLocation) then
+		local currentXP, totalLevelXP = C_AzeriteItem.GetAzeriteItemXPInfo(azeriteItemLocation)
+		str = str .. HeartOfAzerothTooltip_NumberFormat(((currentXP / totalLevelXP) * 100), 2) .. "%"
+		str = str .. " (" .. currentXP .. "/" .. totalLevelXP .. ")"
+		return str
+	end
+	return nil
+end
+
+function HeartOfAzerothTooltip_OnTooltipSetItem(tooltip)
+	if (tooltip:GetItem() == "Heart of Azeroth") then
+		local progressString = HeartOfAzerothTooltip_GetAzeriteProgressString()
+		if (progressString == nil) then
+			return
+		end
+		GameTooltip:AddDoubleLine("Azerite Power:", progressString, 0.4, 0.7, 1, 0.4, 0.7, 1)
+	end
+end
+
+if (UnitLevel("player") == 120) then
+	GameTooltip:HookScript("OnTooltipSetItem", HeartOfAzerothTooltip_OnTooltipSetItem)
+end
---
+++ README.md
@@ -0,0 +1,26 @@
+HeartOfAzerothTooltip
+-------------
+Interface add-on for World of Warcraft: Battle for Azeroth.
+
+This add-on expands the in-game tooltip when hovering the Heart of Azeroth item to reveal the current progress on in as a percentage and "x/y".
+
+![Heart of Azeroth with progress in tooltip](assets/example.jpg)
+
+By default, the experience bar can be configured in-game by players to show the Azerite progress of your Heart of Azeroth item. However, if your experience bar is hidden, e.g. if using a completely different UI package, you no longer have the luxury of monitoring your Azerite levels. This add-on combats that discrepancy.
+
+# Installation
+
+Copy the folder `Interface/AddOns/HearthOfAzerothTooltip` to `<WOWINSTALL>/Interface/AddOns/HearthOfAzerothTooltip`, where `<WOWINSTALL>` is the location of your World of Warcraft installation directory, e.g.:
+
+- On Windows: `C:\Program Files (x86)\World of Warcraft`
+- On Mac: `/Application/World of Warcraft`
+
+# Credits
+
+## Author
+
+- Kasper Søfren (soefritz@gmail.com)
+
+## Inspiration
+
+Largely based on Adamod: http://www.wowinterface.com/downloads/info24741-Adamod.html
